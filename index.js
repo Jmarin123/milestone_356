0149258e8db79f11f270aacb72e15565ddd82196
Rosseleen Nunez <nrosseleen@gmail.com>
2022-11-08 14:09:41 -0500

    git merge conflict, readded the cookieParser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const Y = require('yjs');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
